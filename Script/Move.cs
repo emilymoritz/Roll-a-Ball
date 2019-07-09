b8c63bf55cec5265956e1a7a27744a04490db37e
emilymoritz <52431807+emilymoritz@users.noreply.github.com>
2019-07-09 19:24:44 -0400

    Add files via upload

---
+++ Script/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float min = 2f;
+    public float max = 3f;
+
+    void Start()
+    {
+
+        min = transform.position.x;
+        max = transform.position.x + 6;
+
+    }
+    void Update()
+    {
+
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+
+    }
+
+}
---
+++ Script/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text scoreText;
+    public Text livesText;
+    public Object player;
+
+    private Rigidbody rb;
+    private int count;
+    private int score;
+    private int lives;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        lives = 3;
+        SetCountText();
+        SetScoreText();
+        SetLivesText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            count = count + 1;
+            SetScoreText();
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score - 1;
+            lives = lives - 1;
+            SetScoreText();
+            SetCountText();
+            SetLivesText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+    }
+
+    void SetScoreText()
+
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 12)
+        {
+            winText.text = "You've Won!";
+        }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            winText.text = "You Died!";
+            Destroy(player);
+
+        }
+
+    }
+}
+  
